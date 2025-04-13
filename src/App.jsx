9ed9312245b452f18abbbe32154db4fbb098b243
Daniel <callejasmark63@gmail.com>
2025-04-13 23:51:24 +0800

    ❄️ Build error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Home from './pages/home'
+import Home from './pages/Home'
 import './App.css'
 import BookProvider from './context/BookProvider'
 import ExpenseProvider from './context/ExpenseProvider'
